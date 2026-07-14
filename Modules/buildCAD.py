930c5147ee110ce1c1fd16a90338179933c0c271
QPienaar <quinten.pienaar@gmail.com>
2026-07-14 15:35:54 -0400

    changing interferencia mode from slice to common

--- Modules/buildCAD.py
+++ Modules/buildCAD.py
@@ -7,7 +7,7 @@ from .Utils.booleanFunction import BoolSequence
 from .Utils.boundBox import myBox
 
 
-def interferencia(container, cell, mode="slice"):
+def interferencia(container, cell, mode="common"):
 
     if mode == "common":
         return cell.shape.common(container.shape)
